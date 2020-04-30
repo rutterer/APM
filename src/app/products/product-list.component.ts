401ebbb379758a25f14f49a281f3d18f314a300a
Derek Rutter <derek.rutter@ofm.wa.gov>
2020-04-30 16:03:15 -0700

    added nested components, put data in a service

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,48 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+
+export class ProductListComponent implements OnInit {
+    
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    products: any[] = [
-      {
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2019",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "assets/images/leaf_rake.png"
-      },
-      {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2019",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "assets/images/garden_cart.png"
-      },
-      {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2019",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "assets/images/hammer.png"
-      }
-    ];
+
+     _listFilter: string;
+     get listFilter(): string {
+       return this._listFilter;
+     }
+     set listFilter(value:string) {
+       this._listFilter = value;
+       this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+     }
+
+    filteredProducts: IProduct[];
+    products: IProduct[] = [];
+
+    constructor(private productService: ProductService) {
+      
+    }
+
+    onRatingClicked(message: string): void {
+      this.pageTitle = 'Product List ' + message;
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+      filterBy = filterBy.toLocaleLowerCase();
+      return this.products.filter((product: IProduct) =>
+                product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
 
     toggleImage(): void {
       this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+      this.products = this.productService.getProducts();
+      this.filteredProducts = this.products;
     }
 }
---
+++ src/app/shared/convert-to-spices.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'convertToSpaces'
+})
+
+export class ConvertToSpacesPipe implements PipeTransform {
+    
+    
+    transform(value: string, character: string) {
+        
+        return value.replace(character, ' ');
+
+    }
+
+}
